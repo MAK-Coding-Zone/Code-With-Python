95bb6bd56fb1fe48425fc0dc614148606e4a7753
MuzammilAliKhan62 <164666108+MuzammilAliKhan62@users.noreply.github.com>
2025-01-08 02:22:26 +0500

    Create main.py

---
+++ Lecture 2/main.py
@@ -0,0 +1,11 @@
+# TO DOWNLOAD PYTHON VISIT THIS SITE 
+# https://www.python.org/
+
+# TO DOWNLOAD VISUAL STUDIO CODE(IDE) VISIT THIS SITE
+# https://code.visualstudio.com/download
+
+print("hello world!")
+
+print("muzammil ali khan")
+print("code with python")
+print("mak coding zone")
