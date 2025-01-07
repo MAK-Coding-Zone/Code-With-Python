e482ca6d53db85dcc18415d6dfd106f0053b48a4
MuzammilAliKhan62 <164666108+MuzammilAliKhan62@users.noreply.github.com>
2025-01-07 17:50:53 +0500

    Create main.py

---
+++ lecture 3/main.py
@@ -0,0 +1,25 @@
+# STRING
+name = "muzammil ali khan"
+price="45"
+print(type(name))
+print(type(price))
+
+total=  50 + 10
+print(total)
+# OUTPUT: <class 'str'>
+
+# INTEGERS
+age = 45
+print(type(age))
+# OUTPUT: <class 'int'>
+
+# FLOAT
+marks = 90.78
+print(type(marks))
+# OUTPUT: <class 'float'>
+
+#BOOLEAN
+python=True
+print(type(python))
+# OUTPUT: <class 'bool'>    
+
