be05f0cfe6c85058e5de752088e24c7017043a80
MuzammilAliKhan62 <164666108+MuzammilAliKhan62@users.noreply.github.com>
2025-01-08 02:17:42 +0500

    Create main.py

---
+++ Lecture 1/main.py
@@ -0,0 +1,7 @@
+# Save this playlist for python 
+
+# CODE WITH PYTHON
+# https://www.youtube.com/playlist?list=PLPi_BWpL0qr1EmVkAiJv9BofEpOPuqf9F
+
+# Subscribe the youtube channel
+# https://www.youtube.com/@MAKCodingZone
