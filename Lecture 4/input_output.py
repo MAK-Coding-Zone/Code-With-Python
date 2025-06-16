8ffe0a0bb5651283291704d1ec2ea3aa24594af2
MuzammilAliKhan62 <164666108+MuzammilAliKhan62@users.noreply.github.com>
2025-06-16 18:51:04 +0500

    Create input_output.py

---
+++ Lecture 4/input_output.py
@@ -0,0 +1,20 @@
+# name = "muzammil ali khan"
+# print(name)
+
+
+# input
+# name = eval(input("enter your name: "))
+# # outout
+# print(type(name))
+
+
+# name = input("enter your name: ")
+# # outout
+
+
+# print("hello " + name  + "how are you?")
+
+# print("hello" , "i am muzammil")
+print("muzammil" , "ali" , "khan" , sep="-")
+# print("hello")
+
