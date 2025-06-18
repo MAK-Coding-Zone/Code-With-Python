936d43c1ab2bd11d3b9e251013a82e971be6ca67
MuzammilAliKhan62 <164666108+MuzammilAliKhan62@users.noreply.github.com>
2025-06-18 19:18:14 +0500

    Create comments_and_keywords.py

---
+++ Lecture 5/comments_and_keywords.py
@@ -0,0 +1,12 @@
+# single line comment
+# This is a single line comment in Python
+
+
+'''multi line comment
+python comments
+print("i am uncomment")
+print("mak coding zone")'''
+
+
+import keyword
+print(keyword.kwlist)
