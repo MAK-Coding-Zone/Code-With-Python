5cb2e4f4557fd7f961b0b859feaf2f270df497e5
MuzammilAliKhan62 <164666108+MuzammilAliKhan62@users.noreply.github.com>
2025-06-23 21:48:06 +0500

    Create datatypes.py

---
+++ Lecture 6/datatypes.py
@@ -0,0 +1,53 @@
+
+# variables
+# name = "muzammil ali khan"
+# print(name)
+# print(type(name))
+
+
+# string
+# name = "muzammil ali khan"
+# print(name)
+# print(type(name))
+
+# number 
+# age = 90
+# print(age)
+# print(type(age))
+
+# float 
+# height = 5.3
+# print(height)
+# print(type(height))
+
+# boolean
+# The code snippet `is_student = True` is assigning the boolean value `True` to the variable
+# `is_student`.
+# is_student = True
+# is_student = False
+# print(is_student)
+# print(type(is_student))
+
+# list 
+# list = ["muzammil " , 1 , 20.9, True,1]
+# print(list)
+# print(type(list))
+
+# tuple 
+# tuple = ("muzammil " , 1 , 20.9, True)
+# print(tuple)
+# print(type(tuple))
+
+# set  
+# set =  {"muzammil " , 1 , 20.9, True, 1}
+# print(set)
+# print(type(set))
+
+# dictionary
+# dic = {
+#     "name" : "muzammil ali khan",
+#     "age" : 20,
+#     "name" : "saqib"
+# }
+# print(dic)
+# print(type(dic))
